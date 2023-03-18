e82bfdbddb22a55595c76b5fb52eee8600bed3be
Sukruth Surendra N S <Sukruth.sns@campusuvce.in>
2023-03-18 12:05:31 +0530

    soln 5

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,6 +1,17 @@
 package com.driver;
 
 public class Main {
+    public static class A {
+        public String meth() {
+            return "Invoking method from class A";
+        }
+    }
+    public static class B extends A {
+        @Override
+        public String meth() {
+            return "Method is overridden in Extended class B";
+        }
+    }
     public static void main(String[] args) {
         A Obja = new A();
         System.out.println(Obja.meth());
