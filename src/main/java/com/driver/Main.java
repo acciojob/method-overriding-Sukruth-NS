05d65991a19d41c8d100a480efe51a2ca6f73734
Sukruth Surendra N S <Sukruth.sns@campusuvce.in>
2023-03-18 11:52:05 +0530

    second commit

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,20 +1,5 @@
 package com.driver;
 
-// Step 1
-class A {
-    public String meth() {
-        return "Invoking method from class A";
-    }
-}
-
-// Step 2
-class B extends A {
-    @Override
-    public String meth() {
-        return "Method is overridden in Extended class B";
-    }
-}
-
 public class Main {
     public static void main(String[] args) {
         A aObj = new B();
